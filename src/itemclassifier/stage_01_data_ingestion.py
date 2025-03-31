112b3a7906ec3cd384f4bd8887467dff6468e72a
Leonardo Garcia <67179308+lhgarciadev@users.noreply.github.com>
2025-03-31 18:05:40 -0500

    Update stage_01_data_ingestion.py

--- src/itemclassifier/stage_01_data_ingestion.py
+++ src/itemclassifier/stage_01_data_ingestion.py
@@ -235,9 +235,4 @@ def build_dataset():
 
 
 if __name__ == "__main__":
-    X_train, y_train, X_test, y_test, _, _, _ = build_dataset()
-    print(f"X_train shape: {X_train.shape}")
-    print(f"y_train shape: {y_train.shape}")
-    print(f"X_test shape: {X_test.shape}")
-    print(f"y_test shape: {y_test.shape}")
-    
+    build_dataset()
